fc99c7e5ad7ff92816a46dcdc8d9b6e667c2fece
Sebastian Zavala <mrjchzz@gmail.com>
2025-12-16 20:57:05 -0600

    docs: ideas

--- src/data/boosts.js
+++ src/data/boosts.js
@@ -2,28 +2,41 @@ export const BOOSTS = [
   {
     id: 'x2',
     type: 'multiplier',
-    cost: 1, // test. change in production
+    cost: 10, // test. change in production
     factor: 2,
   },
   {
     id: 'x3',
     type: 'multiplier',
-    cost: 1, // test. change in production
+    cost: 40, // test. change in production
     factor: 3,
     requires: 'x2',
   },
   {
     id: 'x5',
     type: 'multiplier',
-    cost: 1, // test. change in production
+    cost: 100, // test. change in production
     factor: 5,
     requires: 'x3',
   },
   {
     id: 'dvd',
     type: 'visual',
-    cost: 1, // test. change in production
+    cost: 75, // test. change in production
     requires: 'x3',
     visual: 'dvd',
   },
 ];
+
+/* 
+IDEAS
+
+subway surfers gameplay
+minecraft parkour gameplay
+corte de jabon
+asrm visual
+notificaciones
+barra de progreso reiniciandose
+timer pomodoro (solo visual, no afecta en nada)
+
+*/
